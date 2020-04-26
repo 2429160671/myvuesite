e68abb1cf0fed20b603c4b6c9d9a60ea33d047dc
ennio <2429160671@qq.com>
2020-04-26 19:12:04 +0800

    'add'

--- myvuesite/settings.py
+++ myvuesite/settings.py
@@ -73,8 +73,7 @@ WSGI_APPLICATION = 'myvuesite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-import pymysql
-pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
