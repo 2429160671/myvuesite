20d6a348d7c27a999100e90b12d0576315f9595f
ennio <2429160671@qq.com>
2020-04-26 19:25:12 +0800

    'add'

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import pymysql
 
 
 def main():
@@ -18,4 +19,5 @@ def main():
 
 
 if __name__ == '__main__':
+    pymysql.install_as_MySQLdb()
     main()
