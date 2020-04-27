a11f41b36aacf43672f6e245a4937f6ef610436f
ennio <2429160671@qq.com>
2020-04-27 13:09:19 +0800

    'add'

--- myvuesite/settings.py
+++ myvuesite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
         'NAME': 'vue',         # 你要存储数据的库名，事先要创建之
         'USER': 'root',         # 数据库用户名
-        'PASSWORD': 'wen52010',     # 密码
+        'PASSWORD': 'Ennio@123',     # 密码
         'HOST': 'localhost',    # 主机
         'PORT': '3306',         # 数据库使用的端口
     }
